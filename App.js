f80f482f4c8c46925a84b26bac34e32b8334daf2
Ganvs <gustavo.anverze@gmail.com>
2019-10-06 13:54:28 -0300

    app.js structure

--- App.js
+++ App.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { AppLoading, Asset } from "expo";
+
+import Navigation from "./navigation";
+import { Block } from "./components";
+
+// import all used images
+const images = [
+  require("./assets/icons/back.png"),
+  require("./assets/icons/plants.png"),
+  require("./assets/icons/seeds.png"),
+  require("./assets/icons/flowers.png"),
+  require("./assets/icons/sprayers.png"),
+  require("./assets/icons/pots.png"),
+  require("./assets/icons/fetilizers.png"),
+  require("./assets/images/plants_1.png"),
+  require("./assets/images/plants_2.png"),
+  require("./assets/images/plants_3.png"),
+  require("./assets/images/explore_1.png"),
+  require("./assets/images/explore_2.png"),
+  require("./assets/images/explore_3.png"),
+  require("./assets/images/explore_4.png"),
+  require("./assets/images/explore_5.png"),
+  require("./assets/images/explore_6.png"),
+  require("./assets/images/avatar.png")
+];
+
+export default class App extends Component {
+  state = {
+    isLoadingComplete: false
+  };
+
+  handleResourcesAsync = async () => {
+    // caching all the images
+    // for better performance on the app
+
+    const cacheImages = images.map(img => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all(cacheImages);
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.handleResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <Navigation />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- navigation/index.js
+++ navigation/index.js
@@ -3,22 +3,22 @@ import { Image } from "react-native";
 import { createAppContainer, createStackNavigator } from "react-navigation";
 
 import Welcome from "../screens/Welcome";
-import Login from "../screens/Login";
-import Explore from "../screens/Explore";
-import Browse from "../screens/Browse";
-import Product from "../screens/Product";
-import Settings from "../screens/Settings";
+//import Login from "../screens/Login";
+//import Explore from "../screens/Explore";
+//import Browse from "../screens/Browse";
+//import Product from "../screens/Product";
+//import Settings from "../screens/Settings";
 
 import { theme } from "../constants";
 
 const screens = createStackNavigator(
   {
-    Welcome,
-    Login,
-    Explore,
-    Browse,
-    Product,
-    Settings
+    Welcome
+    //Login,
+    //Explore,
+    //Browse,
+    //Product,
+    //Settings
   },
   {
     defaultNavigationOptions: {
