d1a9855753416c40e959c38d486ea54732514451
Ganvs <gustavo.anverze@gmail.com>
2019-10-06 13:03:07 -0300

    first commit

---
+++ constants/mocks.js
@@ -0,0 +1,54 @@
+const categories = [
+  {
+    id: "plants",
+    name: "Plants",
+    tags: ["products", "inspirations"],
+    count: 147,
+    image: require("../assets/images/plants.png")
+  },
+  {
+    id: "seeds",
+    name: "Seeds",
+    tags: ["products", "shop"],
+    count: 16,
+    image: require("../assets/images/seeds.png")
+  },
+  {
+    id: "flowers",
+    name: "Flowers",
+    tags: ["products", "inspirations"],
+    count: 68,
+    image: require("../assets/images/flowers.png")
+  },
+  {
+    id: "sprayers",
+    name: "Sprayers",
+    tags: ["products", "shop"],
+    count: 17,
+    image: require("../assets/images/sprayers.png")
+  },
+  {
+    id: "pots",
+    name: "Pots",
+    tags: ["products", "shop"],
+    count: 47,
+    image: require("../assets/images/pots.png")
+  },
+  {
+    id: "fertilizers",
+    name: "Fertilizers",
+    tags: ["products", "shop"],
+    count: 9,
+    image: require("../assets/images/fertilizers.png")
+  }
+];
+
+const products = [];
+
+const explore = [
+  //images
+];
+
+const profile = {};
+
+export { categories, products, profile };
